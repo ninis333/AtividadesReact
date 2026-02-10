c83eac1f93322ca2a75a141f5c9f6f89774be1da
ninis333 <ninise755@gmail.com>
2026-02-10 11:03:02 -0300

    Atividade01

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './styles/theme.css'
+import './styles/global.css'
+export function App() {
+  return (
+    <main>
+      <h1>LOGIN: </h1>
+      <p>email:</p>
+      <input type="text" />
+      <p>senha:</p>
+      <input type="password" />
+      <button>entrar</button>
+    </main>
+  )
+}
